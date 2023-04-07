32674959d795472bc8c0103520cfa3b1e5685ea7
Romeo Cosma <122970841+RoMeRO93@users.noreply.github.com>
2023-04-07 09:47:49 +0300

    Add project files.

---
+++ nume prenume 1.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nume;
+        string prenume;
+
+        Console.WriteLine("Introduceti numele:");
+        nume = Console.ReadLine();
+
+        Console.WriteLine("Introduceti prenumele:");
+        prenume = Console.ReadLine();
+
+        string numeIntreg = prenume + " " + nume;
+
+        
+        Console.WriteLine("Salut, " + prenume + "!");
+        Console.WriteLine("Numele complet este: " + numeIntreg);
+        Console.Read();
+    }
+}
